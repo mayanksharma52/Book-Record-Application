a95136187906139d9a1b27bff090f02ea3329b07
mayanksharma52 <mayank.sharma1_cs22@gla.ac.in>
2025-04-19 21:54:26 +0530

    Initial setup

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const express = require("express");
 
+const { users } = require("./data/users.json");
+
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
 
+/**
+ * Routes Created /users
+ * method: Get
+ * description: get all users
+ * response: json
+ * Access: public
+ * Parameters: none
+ */
+
+app.get("/users", (req, res) => {
+  res.status(200).json({
+    status: true,
+    data: users,
+  });
+});
+
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "server is running successfully",
     data: "hey )-:",
-  });
-});
-
-app.get("*", (req, res) => {
-  res.status(404).json({
-    message: "page not found",
   });
 });
 
